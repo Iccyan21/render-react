75bd55e2a82f4b9483f1caa630b2270c8974c1db
Mizuhara Itsuki <110594150+Iccyan21@users.noreply.github.com>
2023-12-14 11:25:47 +0900

    セカンド

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Posts from './post';
 import PostDetail from './postdetail';
+import PostDetailComponent from './postphoto';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
               <Route path="/posts" element={<Posts />}/>
               <Route path="/posts/:title" element={<PostDetail/>}/>
+              <Route path="/photo/:title" element={<PostDetailComponent/>}/>
             </Routes>
       </BrowserRouter>
       
---
+++ src/postphoto.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+interface Post {
+    title: string;
+    photo: string;
+    movies: { title: string; video: string; }[];
+}
+
+const PostDetailComponent = () => {
+    const [post, setPost] = useState<Post | null>(null);
+    const { title } = useParams<{ title: string }>();
+
+    useEffect(() => {
+        axios.get(`https://django-d0bm.onrender.com/post/post-photo/${title}/`)
+            .then(response => {
+                setPost(response.data);
+            })
+            .catch(error => console.error('Error:', error));
+    }, [title]);
+
+    if (!post) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h2>{post.title}</h2>
+            <img src={`https://django-d0bm.onrender.com${post.photo}`} alt={post.title} style={{ maxWidth: '300px' }} />
+            {post.movies.map((movie, index) => (
+                <div key={index}>
+                    <h3>{movie.title}</h3>
+                    <video controls style={{ maxWidth: '300px' }}>
+                        <source src={`https://django-d0bm.onrender.com${movie.video}`} type="video/mp4" />
+                    </video>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default PostDetailComponent;
